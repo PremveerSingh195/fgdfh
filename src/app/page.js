05efb82c8c2c08267c847c9407888c9d9a253553
PremveerSingh195 <prem.veer2836@gmail.com>
2025-05-24 20:27:53 +0530

    dwfjg

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="flex justify-center items-center  font-semibold text-2xl">Future group developer farm house</h1>
+      <h1 className="flex justify-center items-center  font-semibold text-2xl">Future group developer farm house website under maintainence</h1>
     </div>
   );
 }
