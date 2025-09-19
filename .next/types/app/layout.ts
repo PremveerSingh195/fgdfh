fd5f6f24efe46d7878dbbc5604e242a82a468c39
Premveer Singh <premcallsmaster@gmail.com>
2025-09-19 13:32:55 +0530

    dash

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/premveersingh/Documents/Prem/fgdfh/src/app/layout.js
+// File: C:\Prem\fgdfh\src\app\layout.js
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
